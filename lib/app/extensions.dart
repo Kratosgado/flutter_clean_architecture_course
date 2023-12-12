2332ca849a88d393c95de0044859949f137b0acb
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-12-12 10:10:59 +0000

    extensions and mappers

---
+++ lib/app/extensions.dart
@@ -0,0 +1,28 @@
+
+
+// ignore_for_file: constant_identifier_names
+
+const EMPTY = "";
+const ZERO = 0;
+
+// extension on String
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return EMPTY;
+    } else {
+      return this!;
+    }
+  }
+}
+
+// extension on Integer
+extension NonNullInteger on int? {
+  int orZero() {
+    if (this == null) {
+      return ZERO;
+    } else {
+      return this!;
+    }
+  }
+}
